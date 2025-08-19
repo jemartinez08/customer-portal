9adccf3f02eae7c5ff999226d9b0ef9d713b9eab
Jesús Emmanuel Martínez García <je.martinez@softtekprojects.com>
2025-08-19 14:54:10 -0600

    [add] first charts with servicenow data

--- src/app/components/servicenow-test/servicenow-test.component.ts
+++ src/app/components/servicenow-test/servicenow-test.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+// Charts and graphics libraries
+// Importamos las librerías
+import crossfilter from 'crossfilter2';
+import * as dc from 'dc';
+import * as d3 from 'd3';
+
 // Local Imports
 import { ServicenowService } from '../../services/servicenow.service';
 import { environment } from '../../../environments/environment.development';
@@ -14,7 +20,54 @@ export class ServicenowTestComponent implements OnInit {
 
   constructor(private servicenowService: ServicenowService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  initCharts() {
+    // api data from servicenow
+    const apiData = this.data.result;
+    console.log('Datos de la api: ', apiData);
+
+    // 1. Initialize crossfilter
+    const ndx = crossfilter(apiData);
+
+    // 2. Create dimentions
+    const numberDim = ndx.dimension((d: any) => d.priority);
+    const priorityDim = ndx.dimension((d: any) => d.priority);
+    const openedDim = ndx.dimension((d: any) => d.opened_at);
+    const closedDim = ndx.dimension((d: any) => d.closed_at);
+    const stateDim = ndx.dimension((d: any) => d.state);
+    const subcategoryDim = ndx.dimension((d: any) => d.subcategory);
+    const severityDim = ndx.dimension((d: any) => d.severity);
+    const closeCodeDim = ndx.dimension((d: any) => d.close_code);
+
+    // 3. Create groups
+    const priorityGroup = priorityDim.group();
+    const stateGroup = stateDim.group();
+    const subcategoryGroup = subcategoryDim.group();
+    const severityGroup = severityDim.group();
+    const closeCodeGroup = closeCodeDim.group();
+
+    // Pie chart by priotiry
+    const priorityChart = dc.pieChart('#priority-chart');
+    priorityChart
+      .dimension(priorityDim)
+      .group(priorityGroup)
+      .innerRadius(30)
+      .legend(dc.legend());
+
+    // Table of incidents
+    const incidentTable = dc.dataTable('#incident-table');
+    incidentTable
+      .dimension(numberDim)
+      .columns(['number', 'priority', 'opened_at'])
+      .sortBy((d: any) => d.number)
+      .order(d3.ascending);
+
+    // Render all the graphic items
+    dc.renderAll();
+  }
 
   loadData() {
     const payload = {
@@ -23,14 +76,14 @@ export class ServicenowTestComponent implements OnInit {
       password: environment.servicenow.password,
       table: 'incident',
       queryParams: {
-        sysparm_limit: 2,
+        sysparm_limit: 10,
       },
     };
 
     this.servicenowService.queryServiceNow(payload).subscribe({
       next: (res) => {
         this.data = res;
-        console.log(this.data.result);
+        this.initCharts();
       },
       error: (err) => {
         console.error('Error al consultar ServiceNow:', err);
