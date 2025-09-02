6f4c04e46a3be4e634cf8e5c79aee1119d717464
Jesús Emmanuel Martínez García <je.martinez@softtekprojects.com>
2025-09-01 23:04:03 -0600

    [add] main header component for dashboard pages

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DashboardTestComponent } from "../../components/dashboard-test/dashboard-test.component";
 import { ServicenowTestComponent } from "../../components/servicenow-test/servicenow-test.component";
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [DashboardTestComponent, ServicenowTestComponent],
+  imports: [DashboardTestComponent, ServicenowTestComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
