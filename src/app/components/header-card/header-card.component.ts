52a1033ed0479b09296513a982a4af4708703c2b
Jesús Emmanuel Martínez García <je.martinez@softtekprojects.com>
2025-09-02 19:50:14 -0600

    [add] header-card-component

---
+++ src/app/components/header-card/header-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-card',
+  imports: [],
+  templateUrl: './header-card.component.html',
+  styleUrl: './header-card.component.css'
+})
+export class HeaderCardComponent {
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { HeaderCardComponent } from "../header-card/header-card.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [HeaderCardComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
